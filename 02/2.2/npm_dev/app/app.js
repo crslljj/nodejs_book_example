116eabaa7a3c13bceb44bce3dd065e158fa43995
crslljj <chenrongshang@126.com>
2016-02-22 16:25:27 +0800

    delete some files

--- 02/2.2/npm_dev/app/app.js
+++ 02/2.2/npm_dev/app/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -57,8 +56,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-http.createServer(app).listen(3000,"127.0.0.1");
-console.log("Express server listening on port 3000");
-
+app.listen(3000);
+console.log('http://127.0.0.1:3000/');
 
 module.exports = app;
